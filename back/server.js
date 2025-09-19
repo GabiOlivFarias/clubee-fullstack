86ad504cf0dcf66b6ba1cc06f9d1b33aa738c541
Gabi Farias <you@example.com>
2025-09-18 22:00:19 -0400

    [UPDATE]Adicionei a configuração para Vercel

--- back/server.js
+++ back/server.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config(); // Carrega as chaves secretas do arquivo .env
+const express = require('express'); // importa o express
+const cors = require('cors'); // o cors pra nao dar problema e liberar o front
+const session = require('express-session'); // Importa o sistema que cria os crachás de login
+const passport = require('passport'); // Importa o gerente de autenticação
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //Importa a estratégia específica para conversar com o google
 
-const app = express();
+const app = express(); // aqui eu pego o express pra usar com Node, crio a istancia pro servidor
 const PORT = 3001;
 
 // --- CONFIGURAÇÃO DO CORS (A CORREÇÃO CRÍTICA ESTÁ AQUI) ---
